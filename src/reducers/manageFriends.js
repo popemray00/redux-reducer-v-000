a2162d87c52e10aeb4abfe7cb4db1467df688e89
Michael Pope <popemray1986@gmail.com>
2019-01-12 20:22:13 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      let new_friends = state.friends.filter( el => el.id !== action.id );
+      return {friends: new_friends};
+
+      break;
+    default:
+      return state;
+
+  }
 }
