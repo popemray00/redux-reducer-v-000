b9575017f98a5d34ed0d1a1eda79a034723b3b1c
Michael Pope <popemray1986@gmail.com>
2019-01-12 20:21:56 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,8 +3,8 @@ export function manageFriends(state, action){
     case "ADD_FRIEND":
       return {friends: [...state.friends, action.friend]}
     case "REMOVE_FRIEND":
-      
-     return {friends: state.friends}
+      let new_friends = state.friends.filter( el => el.id !== action.id );
+      return {friends: new_friends};
 
       break;
     default:
