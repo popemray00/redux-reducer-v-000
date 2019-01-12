7bffa5c8ae6a64430fc7ba3f607647df8a6f94d9
Michael Pope <popemray1986@gmail.com>
2019-01-12 20:04:44 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,7 +2,6 @@ export function managePresents(state, action){
   switch (action.type) {
     case "INCREASE":
       return {numberOfPresents: state.numberOfPresents + 1}
-
       break;
     default:
       return state;
