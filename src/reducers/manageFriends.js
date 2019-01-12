73659c26c34fb921cf1b18aed8a39cd7d495b0fe
Michael Pope <popemray1986@gmail.com>
2019-01-12 20:20:14 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case "ADD_FRIEND":
-      return {friends: state.friends}
+      return {friends: [...state.friends, action.friend]}
     case "ADD_FRIEND":
      return {friends: state.friends}
 
